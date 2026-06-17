616ebbbfc614e014baea6744568632f1f4d2dd75
Eunice M. <munseey@icloud.com>
2026-06-17 11:18:17 +0200

    refactor SearchInput component styles and structure

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -3,8 +3,8 @@ import '../styles/SearchInput.css';
 export function SearchInput(){
     return (
         <div className="search">
-            <input type="text" placeholder="Enter the link to access your notes ..." style={{height: '2em', width: '90%', borderRadius:'5px'}}></input>
-            <button style={{backgroundColor:'#1F845A' , color: 'white', borderRadius: '5px'}}>➤</button>
+            <input className='link-input' type="text" placeholder="Enter the link to access your notes ..."></input>
+            <button className='submit'>➤</button>
         </div>
         
     )
